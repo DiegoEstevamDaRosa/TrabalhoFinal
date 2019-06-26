1d6cba26919d542ac5aedb0a889a57a075756cf5
DiegoEstevamDaRosa <diego.rosa8@unisul.br>
2019-06-26 20:00:51 -0300

    RESOLVIDO PROBLEMA DA CRIAÇÃO DO BANCO DE DADOS

--- src/main/java/br/unisul/pweb/diego/trabalho/services/DbService.java
+++ src/main/java/br/unisul/pweb/diego/trabalho/services/DbService.java
@@ -1,7 +1,21 @@
 package br.unisul.pweb.diego.trabalho.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.unisul.pweb.diego.trabalho.repositories.DepartamentoRepository;
+import br.unisul.pweb.diego.trabalho.repositories.EmpresaRepository;
+import br.unisul.pweb.diego.trabalho.repositories.VagaRepository;
+
 public class DbService {
 
+	@Autowired
+	private EmpresaRepository empRep;
+	@Autowired
+	private DepartamentoRepository depRep;
+	@Autowired 
+	private VagaRepository vagaRep;
+	
+	
 	public void inicializaBancoDeDados() {
 	
 	}
